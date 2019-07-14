22c25927bfd6a30c1cfec8d08e443819d439bac4
郭奕 <guoyi@163.com>
2019-07-14 21:30:55 +0800

    wepack(一)学习完毕

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("测试webpack打包js文件");
+// 全局引入
+// import '@babel/polyfill'
+
+// // 测试 ES6 语法是否通过 babel 转译
+// const array = [1, 2, 3]
+// const isES6 = () => console.log(...array)
+
+// isES6()
+
+// const arr = [new Promise(() => {}), new Promise(() => {})]
+
+// arr.map(item => {
+//   console.log(item)
+// })
+
+import _ from 'lodash'
+
+console.log(_.join(['a', 'b', 'c']))
+
